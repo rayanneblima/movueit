24e5a20a16b8c8cb2f35e8e612e893a1c60368c5
TEKSYSTEM\Rayanne Lima <rayannelima@teksystem.com.br>
2021-04-14 16:28:38 -0300

    Aplication finished

---
+++ components/organisms/Card.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="bg-white rounded-md flex flex-1 w-full mt-8 lg:mt-0 shadow-sm">
+    <Challenge v-if="currentChallenge" v-bind="currentChallenge" />
+    <StartCycle v-else />
+    <LevelUpModal />
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import { mapGetters } from 'vuex';
+
+import Challenge from '../molecules/Challenge.vue';
+import StartCycle from '../atoms/StartCycle.vue';
+import LevelUpModal from '../atoms/LevelUpModal.vue';
+
+export default Vue.extend({
+  components: {
+    Challenge,
+    StartCycle,
+    LevelUpModal,
+  },
+  computed: {
+    ...mapGetters("Challenges", ['currentChallenge']),
+  },
+})
+</script>
---
+++ store/Challenges/index.ts
@@ -0,0 +1,15 @@
+import allChallenges from '~/assets/challenges/data';
+import { State } from './types';
+
+export const state = (): State => ({
+  level: 1,
+  xp: {
+    current: 0,
+    start: 0,
+    end: 64
+  },
+  completedChallenges: 0,
+  currentChallengeIndex: null,
+  isLevelUpModalOpen: false,
+  allChallenges
+});
